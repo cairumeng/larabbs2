a6d9201a6003b55a02be93933cc9e51f27717c40
cairumeng <cairumeng@gmail.com>
2020-08-28 18:31:07 +0200

    Categories table

---
+++ database/migrations/2020_08_28_160436_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->index()->comment('category name');
+            $table->text('description')->nullable()->comment('category description');
+            $table->integer('post_count')->default(0)->comment('posts count in each category');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
